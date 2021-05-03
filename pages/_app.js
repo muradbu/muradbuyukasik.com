67ece3b66a86cd1d379b5590b591b89a1c21fc4b
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-05-03 08:42:24 +0200

    Init Next

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import "../style/style.css";
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1>Murad Buyukasik</h1>
+            <h2>Murad Buyukasik</h2>
+            <h3>Murad Buyukasik</h3>
+            <h4>Murad Buyukasik</h4>
+            <h5>Murad Buyukasik</h5>
+            <h6>Murad Buyukasik</h6>
+        </div>
+    )
+}
+
+export default index
