b8085e3cd12ef0e8e5cfc3da3749f46423442a80
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-05-04 17:06:50 +0200

    Created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import SocialLinks from '../components/SocialLinks'
+import Card from '../components/Card'
+import Head from 'next/head'
+
+const headerClasses = `
+    text-3xl
+    font-extrabold
+    text-white
+`
+
+const paragraphClasses = `
+    text-lg
+    text-white
+`
 
 const index = () => {
     return (
-        <div>
-            <h1>Murad Buyukasik</h1>
-            <h2>Murad Buyukasik</h2>
-            <h3>Murad Buyukasik</h3>
-            <h4>Murad Buyukasik</h4>
-            <h5>Murad Buyukasik</h5>
-            <h6>Murad Buyukasik</h6>
-        </div>
+        <>
+            <Head>
+                <title>Murad Büyükaşık</title>
+            </Head>
+            <Card>
+                <h1 className={headerClasses}>Hi! My name's Murad,</h1>
+                <p className={paragraphClasses}>
+                    a fullstack developer based in the Netherlands.
+                </p>
+                <p className={`${paragraphClasses} mt-5`}>
+                    This website is a work-in-progress and it's source can be found <a href="https://github.com/MuradBuyukasik/muradbuyukasik.com" className="underline">here</a>.
+                </p>
+                <p className={`${paragraphClasses} my-5`}>
+                    I will use this website to list my portfolio projects, resume, and to write my blog!
+                </p>
+                <SocialLinks />
+            </Card>
+        </>
     )
 }
 
