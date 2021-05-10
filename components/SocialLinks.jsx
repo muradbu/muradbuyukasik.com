d2ac606ab0ee946e636605ae4a06864607b13d24
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-05-10 23:00:02 +0200

    Bug fix and added YouTube link

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube'
 
 const SocialLinks = () => {
     return (
-        <div class="flex space-x-4">
-            <a href="https://github.com/MuradBuyukasik"><GitHubIcon style={{color: '#fff'}}/></a>
-            <a href="https://instagram.com/muradbuyukasik"><InstagramIcon style={{color: '#fff'}}/></a>
+        <div className="flex space-x-4">
+            <a href="https://github.com/MuradBuyukasik"><GitHubIcon style={{ color: '#fff' }} /></a>
+            <a href="https://instagram.com/muradbuyukasik"><InstagramIcon style={{ color: '#fff' }} /></a>
+            <a href="https://www.youtube.com/channel/UC5NC9KmhbG7r5dzPOBP9QMQ"><YouTubeIcon style={{ color: '#fff' }} /></a>
         </div>
     )
 }
