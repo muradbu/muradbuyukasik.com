5765187a89b66b338e943e2637a5dbb5d2dd94b4
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2023-11-05 17:25:11 +0100

    render html

--- api/socials.go
+++ api/socials.go
@@ -1,15 +1,24 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/muradbu/muradbuyukasik.com/components"
+	"github.com/muradbu/muradbuyukasik.com/types"
 )
 
-type Link struct {
-	Text string
-	Url string
-}
-
 func Socials(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "")
+	links := []types.Link{
+		{Text: "𝕏", Url: "https://x.com/muradbu"},
+		{Text: "Github", Url: "https://github.com/muradbu"},
+		{Text: "Twitch", Url: "https://twitch.tv/muradbu"},
+		{Text: "YouTube", Url: "https://www.youtube.com/channel/UCLHZDHY0FfcqTjsl5mJLoxA"},
+		{Text: "Instagram", Url: "https://instagram.com/muradbuyukasik"},
+		{Text: "LinkedIn", Url: "https://www.linkedin.com/in/murad-buyukasik/"},
+	}
+
+	socialLink := components.SocialLinkList(links)
+
+	templ.Handler(socialLink)
 }
